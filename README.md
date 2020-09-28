907c0e7999ab595a37ff43b3c8a8a91ac6be4b51
tkaria <tkaria.info@gmail.com>
2020-09-29 04:53:49 +0900

    update gin sample

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # sandbox-wsl-docker-go
 sandbox for wsl, docker, vscode, go
+
+```
+go build
+go run main.go
+curl http://127.0.0.1:3000
+```
--- main.go
+++ main.go
@@ -1,7 +1,60 @@
 package main
 
-import "fmt"
+import (
+	"io"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("hellow world wsl in docker sample")
+	engine := gin.Default()
+	ua := ""
+	// ミドルウェアを使用
+	engine.Use(func(c *gin.Context) {
+		ua = c.GetHeader("User-Agent")
+		c.Next()
+	})
+
+	engine.Static("/static", "./static")
+	engine.LoadHTMLGlob("public/*")
+
+	engine.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", gin.H{
+			// htmlに渡す変数を定義
+			"message": "hello gin",
+		})
+	})
+	engine.GET("/json", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message":    "hello world",
+			"User-Agent": ua,
+		})
+	})
+
+	engine.POST("/upload", func(c *gin.Context) {
+		file, header, err := c.Request.FormFile("image")
+		if err != nil {
+			c.String(http.StatusBadRequest, "Bad request")
+			return
+		}
+		fileName := header.Filename
+		dir, _ := os.Getwd()
+		out, err := os.Create(dir + "\\images\\" + fileName)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer out.Close()
+		_, err = io.Copy(out, file)
+		if err != nil {
+			log.Fatal(err)
+		}
+		c.JSON(http.StatusOK, gin.H{
+			"status": "ok",
+		})
+	})
+
+	engine.Run(":3000")
 }
